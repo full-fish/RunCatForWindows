a038fe3b0b1901009269fc4d1486d9765888d54e
manseon <91870110+full-fish@users.noreply.github.com>
2023-03-14 22:31:07 +0900

    feat

---
+++ RunCat/Program.cs
@@ -0,0 +1,316 @@
+// Copyright 2020 Takuto Nakamura
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+using RunCat.Properties;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Resources;
+using System.ComponentModel;
+
+namespace RunCat
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new RunCatApplicationContext());
+        }
+    }
+
+    public class RunCatApplicationContext : ApplicationContext
+    {
+        private PerformanceCounter cpuUsage;
+        private ToolStripMenuItem runnerMenu;
+        private ToolStripMenuItem themeMenu;
+        private ToolStripMenuItem startupMenu;
+        private NotifyIcon notifyIcon;
+        private string runner = UserSettings.Default.Runner;
+        private int current = 0;
+        private string systemTheme = "";
+        private string manualTheme = UserSettings.Default.Theme;
+        private Icon[] icons;
+        private Timer animateTimer = new Timer();
+        private Timer cpuTimer = new Timer();
+
+
+        public RunCatApplicationContext()
+        {
+            Application.ApplicationExit += new EventHandler(OnApplicationExit);
+
+            SystemEvents.UserPreferenceChanged += new UserPreferenceChangedEventHandler(UserPreferenceChanged);
+
+            cpuUsage = new PerformanceCounter("Processor Information", "% Processor Utility", "_Total");
+            _ = cpuUsage.NextValue(); // discards first return value
+
+            runnerMenu = new ToolStripMenuItem("Runner", null, new ToolStripMenuItem[]
+            {
+                new ToolStripMenuItem("Cat", null, SetRunner)
+                {
+                    Checked = runner.Equals("cat")
+                },
+                new ToolStripMenuItem("Parrot", null, SetRunner)
+                {
+                    Checked = runner.Equals("parrot")
+                },
+                new ToolStripMenuItem("flower", null, SetRunner)
+                {
+                    Checked = runner.Equals("flower")
+                },
+                new ToolStripMenuItem("kickstone", null, SetRunner)
+                {
+                    Checked = runner.Equals("kickstone")
+                },
+                new ToolStripMenuItem("cat2", null, SetRunner)
+                {
+                    Checked = runner.Equals("cat2")
+                },
+                new ToolStripMenuItem("colorfulcat", null, SetRunner)
+                {
+                    Checked = runner.Equals("colorfulcat")
+                },
+                new ToolStripMenuItem("slime", null, SetRunner)
+                {
+                    Checked = runner.Equals("slime")
+                },
+                new ToolStripMenuItem("hios", null, SetRunner)
+                {
+                    Checked = runner.Equals("hios")
+                }
+            });
+
+            themeMenu = new ToolStripMenuItem("Theme", null, new ToolStripMenuItem[]
+            {
+                new ToolStripMenuItem("Default", null, SetThemeIcons)
+                {
+                    Checked = manualTheme.Equals("")
+                },
+                // new ToolStripMenuItem("Light", null, SetLightIcons)
+                // {
+                //     Checked = manualTheme.Equals("light")
+                // },
+                new ToolStripMenuItem("Dark", null, SetDarkIcons)
+                {
+                    Checked = manualTheme.Equals("dark")
+                }
+            });
+
+            startupMenu = new ToolStripMenuItem("Startup", null, SetStartup);
+            if (IsStartupEnabled())
+            {
+                startupMenu.Checked = true;
+            }
+
+            ContextMenuStrip contextMenuStrip = new ContextMenuStrip(new Container());
+            contextMenuStrip.Items.AddRange(new ToolStripItem[]
+            {
+                runnerMenu,
+                themeMenu,
+                startupMenu,
+                new ToolStripMenuItem("Exit", null, Exit)
+            });
+
+            notifyIcon = new NotifyIcon()
+            {
+                Icon = Resources.light_cat_0,
+                ContextMenuStrip = contextMenuStrip,
+                Text = "0.0%",
+                Visible = true
+            };
+
+            UpdateThemeIcons();
+            SetAnimation();
+            CPUTick();
+            StartObserveCPU();
+            current = 1;
+        }
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            UserSettings.Default.Runner = runner;
+            UserSettings.Default.Theme = manualTheme;
+            UserSettings.Default.Save();
+        }
+
+        private bool IsStartupEnabled()
+        {
+            string keyName = @"Software\Microsoft\Windows\CurrentVersion\Run";
+            using (RegistryKey rKey = Registry.CurrentUser.OpenSubKey(keyName))
+            {
+                return (rKey.GetValue(Application.ProductName) != null) ? true : false;
+            }
+        }
+
+        private string GetAppsUseTheme()
+        {
+            string keyName = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+            using (RegistryKey rKey = Registry.CurrentUser.OpenSubKey(keyName))
+            {
+                object value;
+                if (rKey == null || (value = rKey.GetValue("SystemUsesLightTheme")) == null)
+                {
+                    Console.WriteLine("Oh No! Couldn't get theme light/dark");
+                    return "light";
+                }
+                int theme = (int)value;
+                return theme == 0 ? "dark" : "light";
+            }
+        }
+
+        private void SetIcons()
+        {
+            string prefix = 0 < manualTheme.Length ? manualTheme : systemTheme;
+            ResourceManager rm = Resources.ResourceManager;
+            int capacity = 0;
+            if(runner.Equals("cat")) capacity = 5;
+            else if(runner.Equals("parrot")) capacity = 10;
+            else if(runner.Equals("flower")) capacity = 10;
+            else if(runner.Equals("kickstone")) capacity = 5;
+            else if(runner.Equals("cat2")) capacity = 13;
+            else if(runner.Equals("colorfulcat")) capacity = 26;
+            else if(runner.Equals("slime")) capacity = 9;
+            else if(runner.Equals("hios")) capacity = 3;
+            List<Icon> list = new List<Icon>(capacity);
+            for (int i = 0; i < capacity; i++)
+            {
+                list.Add((Icon)rm.GetObject($"{prefix}_{runner}_{i}"));
+            }
+            icons = list.ToArray();
+        }
+
+        private void UpdateCheckedState(ToolStripMenuItem sender, ToolStripMenuItem menu)
+        {
+            foreach (ToolStripMenuItem item in menu.DropDownItems)
+            {
+                item.Checked = false;
+            }
+            sender.Checked = true;
+        }
+
+        private void SetRunner(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = (ToolStripMenuItem)sender;
+            UpdateCheckedState(item, runnerMenu);
+            runner = item.Text.ToLower();
+            SetIcons();
+        }
+
+        private void SetThemeIcons(object sender, EventArgs e)
+        {
+            UpdateCheckedState((ToolStripMenuItem)sender, themeMenu);
+            manualTheme = "";
+            systemTheme = GetAppsUseTheme();
+            SetIcons();
+        }
+
+        private void UpdateThemeIcons()
+        {
+            if (0 < manualTheme.Length)
+            {
+                SetIcons();
+                return;
+            }
+            string newTheme = GetAppsUseTheme();
+            if (systemTheme.Equals(newTheme)) return;
+            systemTheme = newTheme;
+            SetIcons();
+        }
+
+        private void SetLightIcons(object sender, EventArgs e)
+        {
+            UpdateCheckedState((ToolStripMenuItem)sender, themeMenu);
+            manualTheme = "light";
+            SetIcons();
+        }
+
+        private void SetDarkIcons(object sender, EventArgs e)
+        {
+            UpdateCheckedState((ToolStripMenuItem)sender, themeMenu);
+            manualTheme = "dark";
+            SetIcons();
+        }
+        private void UserPreferenceChanged(object sender, UserPreferenceChangedEventArgs e)
+        {
+            if (e.Category == UserPreferenceCategory.General) UpdateThemeIcons();
+        }
+
+        private void SetStartup(object sender, EventArgs e)
+        {
+            startupMenu.Checked = !startupMenu.Checked;
+            string keyName = @"Software\Microsoft\Windows\CurrentVersion\Run";
+            using (RegistryKey rKey = Registry.CurrentUser.OpenSubKey(keyName, true))
+            {
+                if (startupMenu.Checked)
+                {
+                    rKey.SetValue(Application.ProductName, Process.GetCurrentProcess().MainModule.FileName);
+                }
+                else
+                {
+                    rKey.DeleteValue(Application.ProductName, false);
+                }
+                rKey.Close();
+            }
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            cpuUsage.Close();
+            animateTimer.Stop();
+            cpuTimer.Stop();
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+
+        private void AnimationTick(object sender, EventArgs e)
+        {
+            if (icons.Length <= current) current = 0;
+            notifyIcon.Icon = icons[current];
+            current = (current + 1) % icons.Length;
+        }
+
+        private void SetAnimation()
+        {
+            animateTimer.Interval = 200;
+            animateTimer.Tick += new EventHandler(AnimationTick);
+        }
+
+        private void CPUTick()
+        {
+            float s = cpuUsage.NextValue();
+            notifyIcon.Text = $"{s:f1}%";
+            s = 200.0f / (float)Math.Max(1.0f, Math.Min(20.0f, s / 5.0f));
+            animateTimer.Stop();
+            animateTimer.Interval = (int)s;
+            animateTimer.Start();
+        }
+
+        private void ObserveCPUTick(object sender, EventArgs e)
+        {
+            CPUTick();
+        }
+
+        private void StartObserveCPU()
+        {
+            cpuTimer.Interval = 3000;
+            cpuTimer.Tick += new EventHandler(ObserveCPUTick);
+            cpuTimer.Start();
+        }
+
+    }
+}
